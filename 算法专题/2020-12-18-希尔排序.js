126521487f6e949406a701ef03671c7eb592472a
朱虎 <hu.zhu@weimob.com>
2020-12-22 22:03:48 +0800

    2020-12-22

---
+++ 算法专题/2020-12-18-希尔排序.js
@@ -0,0 +1,24 @@
+/**
+ * 标题：希尔排序
+ * 逻辑：获取增量值h，每次h/2进行插入排序直到到h等于1，最后进行一次插入排序
+ * 时间复杂度 O（logn）
+ */
+function shellSort(arr){
+    if (!arr || !Array.isArray(arr) || arr.length === 1) return; // 做好边界判断加分
+    const len = arr.length;
+    let h = 1;
+    while (h < len/2) {
+        h=h*2+1
+    }
+    while (h >= 1) {
+        h = h / 2;
+        for (var i = h; i < len; i++){
+            for (var j = i; i >= h; i -= h){
+                if (arr[j - h] > arr[j]) {
+                    [arr[j - h], arr[j]] = [arr[j], arr[j - h]];
+                }
+            }
+        }
+    }
+    return arr;
+}
